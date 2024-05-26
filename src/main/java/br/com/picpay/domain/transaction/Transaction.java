bb34120af0e20a3f6813345e80942d587f48a3c7
Romeu Lima <romeu.sousadp@gmail.com>
2024-05-26 15:22:31 -0300

    feat: create Transaction Entity

---
+++ src/main/java/br/com/picpay/domain/transaction/Transaction.java
@@ -0,0 +1,36 @@
+package br.com.picpay.domain.transaction;
+
+import br.com.picpay.domain.user.User;
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.annotations.CreationTimestamp;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Table(name = "transactions")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Transaction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private BigDecimal value;
+
+    @ManyToOne
+    @JoinColumn(name = "payer_id")
+    private User payer;
+
+    @ManyToOne
+    @JoinColumn(name = "payee_id")
+    private User payee;
+
+    @CreationTimestamp
+    private LocalDateTime timestamp;
+}
