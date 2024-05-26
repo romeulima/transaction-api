4c69da9f87bbcf1ae65519baf1024b764602a08e
Romeu Lima <romeu.sousadp@gmail.com>
2024-05-26 15:22:03 -0300

    feat: create User Entity

---
+++ src/main/java/br/com/picpay/domain/user/User.java
@@ -0,0 +1,37 @@
+package br.com.picpay.domain.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.EqualsAndHashCode;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@Entity
+@Table(name = "users")
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@EqualsAndHashCode(of = "id")
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String fullName;
+
+    @Column(unique = true)
+    private String document;
+
+    @Column(unique = true)
+    private String email;
+
+    private String password;
+
+    private BigDecimal balance;
+
+    @Enumerated(EnumType.STRING)
+    private UserType userType;
+}
---
+++ src/main/java/br/com/picpay/domain/user/UserType.java
@@ -0,0 +1,7 @@
+package br.com.picpay.domain.user;
+
+public enum UserType {
+
+    COMMON,
+    MERCHANT
+}
