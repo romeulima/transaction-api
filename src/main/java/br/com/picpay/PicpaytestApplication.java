2c47147e35305f4c4b7d990fb0a4a1778f9cfb3c
Romeu Lima <romeu.sousadp@gmail.com>
2024-05-26 15:21:35 -0300

    feat: initial config

---
+++ src/main/java/br/com/picpay/PicpaytestApplication.java
@@ -0,0 +1,13 @@
+package br.com.picpay;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PicpaytestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PicpaytestApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/picpay/PicpaytestApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.picpay;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PicpaytestApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
