ac7e527f915ff3a24bb5cd870fe055baca5e3a1e
Romeu Lima <romeu.sousadp@gmail.com>
2024-05-27 16:16:35 -0300

    feat: refactoring my user service

--- src/main/java/br/com/picpay/services/UserService.java
+++ src/main/java/br/com/picpay/services/UserService.java
@@ -2,12 +2,17 @@ package br.com.picpay.services;
 
 import br.com.picpay.domain.user.User;
 import br.com.picpay.domain.user.UserType;
+import br.com.picpay.domain.user.exceptions.NoBalanceException;
+import br.com.picpay.domain.user.exceptions.UserWithoutAuthorizationException;
+import br.com.picpay.dtos.user.UserRequestDTO;
 import br.com.picpay.repositories.UserRepository;
+import jakarta.persistence.EntityNotFoundException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.math.BigDecimal;
+import java.util.List;
 
 @Service
 @RequiredArgsConstructor
@@ -16,21 +21,31 @@ public class UserService {
     @Autowired
     private final UserRepository userRepository;
 
-    public void validateTransaction(User payer, BigDecimal value) throws Exception {
-        if (payer.getUserType() == UserType.COMMON) {
-            throw new Exception("Lojistas nao podem enviar dinheiro");
+    public void validateTransaction(User payer, BigDecimal value){
+        if (payer.getUserType() == UserType.MERCHANT) {
+            throw new UserWithoutAuthorizationException("Lojistas nao podem enviar dinheiro");
         }
 
         if (payer.getBalance().compareTo(value) < 0) {
-            throw new Exception("Saldo  insuficiente");
+            throw new NoBalanceException("Saldo  insuficiente");
         }
     }
 
-    public User findUserById(Long id) throws Exception {
-        return this.userRepository.findById(id).orElseThrow(() -> new Exception("Usuario nao encontrado"));
+    public User findUserById(Long id){
+        return this.userRepository.findById(id).orElseThrow(() -> new EntityNotFoundException("Usuario nao encontrado"));
+    }
+
+    public User createUser(UserRequestDTO userRequestDTO) {
+        User user = new User(userRequestDTO);
+        this.saveUser(user);
+        return user;
     }
 
     public void saveUser(User user) {
         this.userRepository.save(user);
+    }
+
+    public List<User> getAllUsers() {
+        return this.userRepository.findAll();
     }
 }
